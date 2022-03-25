42994f28ae90bba9ba19f8c033c3b6e593f16353
Cyder <87647653+CyderVII28x03@users.noreply.github.com>
2022-03-25 10:38:47 +0700

    ProjectArcana-12.x

--- aosp_starlte.mk
+++ aosp_starlte.mk
@@ -27,20 +27,22 @@ $(call inherit-product, device/samsung/starlte/device.mk)
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+## Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+## Inherit Project Arcana specific flag
+ARCANA_MAINTAINER := Cyder
+TARGET_SUPPORT_BLUR := false 
+TARGET_BUILD_GRAPHENEOS_CAMERA := true 
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := arrow_starlte
+PRODUCT_NAME := aosp_starlte
 PRODUCT_DEVICE := starlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-# ArrowOS Properties
-DEVICE_MAINTAINER := Baddar90
 
 BUILD_FINGERPRINT := "samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXSHFUJ2:user/release-keys"
 
